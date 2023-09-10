4be2a9c6b49140001c74588632281727f454d5c0
Mrunal Meshram <26838016+mrunal77@users.noreply.github.com>
2023-09-10 14:25:02 +0530

    Initialize round-13

---
+++ src/Application/TodoLists/ExportTodosQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace CleanArchitecture.Application.TodoLists;
+
+public class ExportTodosQuery : IRequest<ExportTodosVm>
+{
+    public int ListId { get; set; }
+}
---
+++ src/Application/WeatherForecasts/GetWeatherForecastsQuery.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+
+namespace CleanArchitecture.Application.WeatherForecasts;
+
+public class GetWeatherForecastsQuery : IRequest<IEnumerable<WeatherForecast>>
+{
+}
